cda7535af18db73654d885e7876fcb5ec8545549
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2023-10-13 19:03:33 -0700

    Adjust marker size.

--- TomTomNavigation/components/icons/WaypointIcon.js
+++ TomTomNavigation/components/icons/WaypointIcon.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const WaypointIcon = () => (
   <svg
-    width="48"
-    height="61"
+    width="39"
+    height="49"
     viewBox="0 0 48 61"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
