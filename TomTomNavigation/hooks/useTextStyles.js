35ca8ac5b247e5924a3b04056a328f0e9182ee6f
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2023-11-27 13:21:27 -0800

    Make primary text size a little smaller.

--- TomTomNavigation/hooks/useTextStyles.js
+++ TomTomNavigation/hooks/useTextStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@fluentui/react";
 const useTextStyles = makeStyles((theme) => ({
   primaryText: {
     fontFamily: "Noto Sans",
-    fontSize: 28,
+    fontSize: 26,
     fontWeight: 500
   },
   primaryUnitsText: {
