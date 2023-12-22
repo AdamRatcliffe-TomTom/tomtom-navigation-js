4204728c07080747275b8026ede4ce9c2beb6c57
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2023-12-22 12:45:31 -0800

    Update to latest map styles.

--- TomTomNavigation/app/AppContext.js
+++ TomTomNavigation/app/AppContext.js
@@ -43,14 +43,14 @@ export default function AppContextProvider({
         street: {
           name: "street",
           label: strings.street,
-          style: `https://api.tomtom.com/style/1/style/25.2.0-1?map=2/basic_street-${theme}&traffic_flow=2/flow_relative-${theme}&traffic_incidents=2/incidents_${theme}&poi=2/poi_${theme}`,
-          styleDriving: `https://api.tomtom.com/style/1/style/25.2.0-1?map=2/basic_street-${theme}-driving&traffic_flow=2/flow_relative-${theme}&traffic_incidents=2/incidents_${theme}&poi=2/poi_${theme}`
+          style: `https://api.tomtom.com/style/1/style/25.2.2-1?map=2/basic_street-${theme}&traffic_flow=2/flow_relative-${theme}&traffic_incidents=2/incidents_${theme}&poi=2/poi_${theme}`,
+          styleDriving: `https://api.tomtom.com/style/1/style/25.2.2-1?map=2/basic_street-${theme}-driving&traffic_flow=2/flow_relative-${theme}&traffic_incidents=2/incidents_${theme}&poi=2/poi_${theme}`
         },
         satellite: {
           name: "satellite",
           label: strings.satellite,
           style:
-            "https://api.tomtom.com/style/1/style/25.2.0-1?map=2/basic_street-satellite&traffic_flow=2/flow_relative-light&traffic_incidents=2/incidents_light&poi=2/poi_light"
+            "https://api.tomtom.com/style/1/style/25.2.2-1?map=2/basic_street-satellite&traffic_flow=2/flow_relative-light&traffic_incidents=2/incidents_light&poi=2/poi_light"
         }
       },
       measurementSystem:
