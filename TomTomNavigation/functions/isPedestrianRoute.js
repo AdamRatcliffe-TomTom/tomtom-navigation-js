37631a7c2a0cc85789c00846b93fbb769ccc7cbb
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2024-11-11 20:09:52 -0800

    Remove resources specific to Power Apps

--- TomTomNavigation/functions/isPedestrianRoute.js
+++
@@ -1,5 +0,0 @@
-const isPedestrianRoute = (route) =>
-  (route?.type === "FeatureCollection" ? route.features[0] : route)?.properties
-    .sections[0].travelMode === "pedestrian";
-
-export default isPedestrianRoute;
