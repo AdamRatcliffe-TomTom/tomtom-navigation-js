ea379ece0e92c68f24d9fa7275e66fb840991eb2
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2024-11-18 17:54:54 -0800

    Remove fallbacks from webpack.config.js and dependencies.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,5 +1,5 @@
 const path = require("path");
-const peerDependencies = require("./package.json").peerDependencies;
+const { peerDependencies } = require("./package.json");
 
 module.exports = {
   entry: "./src/index.js", // Entry point for your library
@@ -16,25 +16,10 @@ module.exports = {
     externals[pkg] = pkg; // Treat each peer dependency as an external
     return externals;
   }, {}),
-  resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
-    fallback: {
-      stream: require.resolve("stream-browserify"),
-      buffer: require.resolve("buffer"),
-      crypto: require.resolve("crypto-browserify"),
-      process: require.resolve("process"),
-      os: require.resolve("os-browserify"),
-      path: require.resolve("path-browserify"),
-      constants: require.resolve("constants-browserify"),
-      vm: require.resolve("vm-browserify"),
-      fs: false // Disable fs module
-    },
-    extensions: [".js", ".jsx", ".ts", ".tsx"] // Resolve these extensions
-  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/, // Transpile JavaScript and JSX
+        test: /\.(js|mjs|cjs|jsx|ts|tsx)$/, // Transpile JavaScript and JSX
         exclude: /node_modules/,
         use: "babel-loader"
       },
